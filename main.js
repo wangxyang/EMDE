c764a22438ca84db2e7193672eab03782f792705
wangxy <1406824781@qq.com>
2024-02-05 16:45:57 +0800

    EMDE-1-新增并设置原生菜单

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 /**
  * 主进程 main process文件 依赖electron 加载window并打开react默认访问地址及端口
  */
-const { app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, Menu} = require('electron')
 const isDev = require('electron-is-dev')
+const menuTemplate = require('./src/electron/MenuTemplate')
 
 //app准备完成即加载主窗口 
 let mainWindow;
@@ -18,4 +19,7 @@ app.on('ready', () => {
     //设置url变量 通过 @electron-is-dev 判断当前开发/生产环境 开发环境加载React默认地址
     const urlLocation = isDev ? 'http://localhost:3000' : 'blank'
     mainWindow.loadURL(urlLocation)
+    //设置原生菜单
+    const menu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(menu)
 })
